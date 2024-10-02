93f551a4964b355003f9a88013eced96ceb18ec6
Damagedjokke <thgl0l@hotmail.com>
2024-10-02 09:44:01 +0200

    g

---
+++ Project/src/models/API.java
@@ -0,0 +1,9 @@
+package models;
+
+public class API {
+    public static final String API_KEY = "b21e588b0b143dc98fdc0834fb90535e";
+
+    public static String getAPI() {
+        return API_KEY;
+    }
+}
