fb218ba666a9f2728ab11fa0429b7ee3cc51c4e1
Aram <aramf@hiof.no>
2024-10-15 19:36:48 +0200

    Added a void method for changing a beskjed

--- App/src/main/java/repository/BeskjedRepository.java
+++ App/src/main/java/repository/BeskjedRepository.java
@@ -5,5 +5,8 @@ import model.Beskjed;
 public interface BeskjedRepository {
     // Metode for å opprette en beskjed
     void oppretteBeskjed(Beskjed beskjed);
+
+    // Metode for å endre beskjed
+    void endreBeskjed(int beskjedId, String Beskrivelse, int synligTidsenhet);
 }
 
