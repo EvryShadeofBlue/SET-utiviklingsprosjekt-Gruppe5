4ade6d7c84fdff213525fa0d9667e73ca4e3fdcd
Aram <aramf@hiof.no>
2024-10-16 18:37:19 +0200

    Implemented the logic for creating a beskjed in the method

---
+++ App/src/main/java/model/BeskjedService.java
@@ -0,0 +1,16 @@
+package model;
+
+import repository.BeskjedRepository;
+
+public class BeskjedService {
+    private BeskjedRepository beskjedRepository;
+
+    public BeskjedService(BeskjedRepository beskjedRepository) {
+        this.beskjedRepository = beskjedRepository;
+    }
+
+    public void opprettBeskjed(String beskrivelse, int synligTidsenhet) {
+        Beskjed beskjed = new Beskjed(beskrivelse, synligTidsenhet);
+        beskjedRepository.oppretteBeskjed(beskjed);
+    }
+}
