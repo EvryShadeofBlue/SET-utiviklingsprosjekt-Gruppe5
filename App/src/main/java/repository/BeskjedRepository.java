8c9e51a3c3cd15501220e5f7c26f30eecc61a7f1
Aram <aramf@hiof.no>
2024-10-20 12:53:45 +0200

    added method to get besked in beskjedrepository

--- App/src/main/java/repository/BeskjedRepository.java
+++ App/src/main/java/repository/BeskjedRepository.java
@@ -1,15 +1,19 @@
 package repository;
 
-import model.Beskjed;
+import models.Beskjed;
 
 public interface BeskjedRepository {
     // Metode for å opprette en beskjed
     void oppretteBeskjed(Beskjed beskjed);
 
     // Metode for å endre beskjed
-    void endreBeskjed(int beskjedId, String Beskrivelse, int synligTidsenhet);
+    void oppdaterBeskjed(int beskjed, String Beskrivelse, int synligTidsenhet);
 
     // Metode for å slette en beskjed
     void slettBeskjed(int beskjedId);
+
+    // Metode for å hente en beskjed
+    // Denne metoden kan vi senere ved implementasjo vurdere om det er fornuftig
+    Beskjed hentBeskjed(int beskjedId);
 }
 
