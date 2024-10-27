e04288f32b31e5a0e2eb59753b9f6a986cf535aa
Aram <aramf@hiof.no>
2024-10-27 12:41:42 +0100

    added content to parorende class

--- App/core/src/main/java/org/app/core/models/Parorende.java
+++ App/core/src/main/java/org/app/core/models/Parorende.java
@@ -1,4 +1,51 @@
 package org.app.core.models;
 
 public class Parorende {
+    private int parorendeId;
+    private String fornavn;
+    private String etternavn;
+    private String tlf;
+    private String epost;
+
+    public Parorende(String fornavn, String etternavn, String tlf, String epost) {
+        this.fornavn = fornavn;
+        this.etternavn = etternavn;
+        this.tlf = tlf;
+        this.epost = epost;
+    }
+
+    public int getParorendeId() {
+        return parorendeId;
+    }
+    public void setParorendeId(int parorendeId) {
+        this.parorendeId = parorendeId;
+    }
+
+    public String getFornavn() {
+        return fornavn;
+    }
+    public void setFornavn(String fornavn) {
+        this.fornavn = fornavn;
+    }
+
+    public String getEtternavn() {
+        return etternavn;
+    }
+    public void setEtternavn(String etternavn) {
+        this.etternavn = etternavn;
+    }
+
+    public String getTlf() {
+        return tlf;
+    }
+    public void setTlf(String tlf) {
+        this.tlf = tlf;
+    }
+
+    public String getEpost() {
+        return epost;
+    }
+    public void setEpost(String epost) {
+        this.epost = epost;
+    }
 }
