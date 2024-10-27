4cff0dcd9f22ba408bb5f32f58febe4742ebb230
Aram <aramf@hiof.no>
2024-10-27 12:36:06 +0100

    Added content for pleietrengende class

--- App/core/src/main/java/org/app/core/models/Pleietrengende.java
+++ App/core/src/main/java/org/app/core/models/Pleietrengende.java
@@ -1,5 +1,27 @@
 package org.app.core.models;
 
 public class Pleietrengende {
+    private int pleietrengendeId;
+    private String fornavn;
+    private String etternavn;
+
+    public Pleietrengende(String fornavn, String etternavn) {
+        this.fornavn = fornavn;
+        this.etternavn = etternavn;
+    }
+
+    public String getFornavn(){
+        return fornavn;
+    }
+    public void setFornavn(String fornavn) {
+        this.fornavn = fornavn;
+    }
+
+    public String getEtternavn() {
+        return etternavn;
+    }
+    public void setEtternavn(String etternavn) {
+        this.etternavn = etternavn;
+    }
 
 }
