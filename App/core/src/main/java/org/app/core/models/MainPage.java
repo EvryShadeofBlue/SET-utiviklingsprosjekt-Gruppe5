f6044ccc29c2d67b2fd6154c99b8e9bf6d900596
Aram <aramf@hiof.no>
2024-10-30 17:15:27 +0100

    Did some minor changes in the logic

--- App/core/src/main/java/org/app/core/models/MainPage.java
+++ App/core/src/main/java/org/app/core/models/MainPage.java
@@ -24,31 +24,61 @@ public class MainPage extends JFrame {
 
         g1.gridx = 0;
         g1.gridy = 0;
-        g1.gridwidth = 1;
-        relativeNameLabel = new JLabel("Deg: ");
+        g1.anchor = GridBagConstraints.NORTHWEST;
+        relativeNameLabel = new JLabel("Deg: " + relativeName);
         add(relativeNameLabel, g1);
 
-        g1.gridx = 1;
-        g1.gridy = 0;
+        /*
+        g1.gridy = 1;
         g1.gridwidth = 2;
         relativeNameValue = new JLabel(relativeName);
         add(relativeNameValue, g1);
 
-        g1.gridx = 0;
+         */
+
+
         g1.gridy = 1;
-        g1.gridwidth = 1;
-        careRecieverNameLabel = new JLabel("Pleietrengende: ");
+        careRecieverNameLabel = new JLabel("Pleietrengende: " + (careRecieverName.isEmpty() ? "Ingen pleietrengende registrert" : careRecieverName));
         add(careRecieverNameLabel, g1);
 
+        /*
         String careRecieverDisplayName = (careRecieverName == null || careRecieverName.isEmpty())
                 ? "Ingen pleietrengende registrert" : careRecieverName;
         careRecieverNameValue = new JLabel(careRecieverDisplayName);
+
+
 
         g1.gridx = 1;
         g1.gridy = 1;
         g1.gridwidth = 2;
         careRecieverNameValue = new JLabel(careRecieverName);
         add(careRecieverNameValue, g1);
+
+         */
+
+        JButton leggTilPleietrengendeKnapp = new JButton("Legg til pleietrengende");
+        JButton avtalerKnapp = new JButton("Avtaler");
+        JButton beskjedKnapp = new JButton("Beskjeder");
+        JButton handlelisteKnapp = new JButton("Handleliste");
+
+        Dimension buttonSize = new Dimension(200, 40);
+        leggTilPleietrengendeKnapp.setPreferredSize(buttonSize);
+        avtalerKnapp.setPreferredSize(buttonSize);
+        beskjedKnapp.setPreferredSize(buttonSize);
+        handlelisteKnapp.setPreferredSize(buttonSize);
+
+        g1.gridx = 0;
+        g1.gridy = 2;
+        add(leggTilPleietrengendeKnapp, g1);
+
+        g1.gridy = 3;
+        add(avtalerKnapp, g1);
+
+        g1.gridy = 4;
+        add(beskjedKnapp, g1);
+
+        g1.gridy = 5;
+        add(handlelisteKnapp, g1);
 
         setVisible(true);
 
