f6a19ec17a5e6e6a0bd331ba539e251d67a49a70
Aram <aramf@hiof.no>
2024-10-30 15:54:09 +0100

    Created a main page after login

--- App/core/src/main/java/org/app/core/models/MainPage.java
+++ App/core/src/main/java/org/app/core/models/MainPage.java
@@ -25,13 +25,20 @@ public class MainPage extends JFrame {
         g1.gridx = 0;
         g1.gridy = 0;
         g1.gridwidth = 1;
-        add(relativeNameLabel = new JLabel("Deg: "), g1);
+        relativeNameLabel = new JLabel("Deg: ");
+        add(relativeNameLabel, g1);
 
         g1.gridx = 1;
         g1.gridy = 0;
         g1.gridwidth = 2;
-        relativeNameLabel = new JLabel(relativeName);
+        relativeNameValue = new JLabel(relativeName);
         add(relativeNameValue, g1);
+
+        g1.gridx = 0;
+        g1.gridy = 1;
+        g1.gridwidth = 1;
+        careRecieverNameLabel = new JLabel("Pleietrengende: ");
+        add(careRecieverNameLabel, g1);
 
         g1.gridx = 1;
         g1.gridy = 1;
