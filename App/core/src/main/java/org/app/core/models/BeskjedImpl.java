e4e99864f5ca78e55ba5d9411ed2225c735bf6f9
Aram <aramf@hiof.no>
2024-10-30 22:14:58 +0100

    created a class to implement sql queries for beskjeder

---
+++ App/core/src/main/java/org/app/core/models/BeskjedImpl.java
@@ -0,0 +1,34 @@
+package org.app.core.models;
+
+import org.app.core.repository.BeskjedRepository;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class BeskjedImpl implements BeskjedRepository {
+    private Connection connection;
+
+    public BeskjedImpl() {
+        try {
+            connection = DriverManager.getConnection(Resources.url, Resources.user, Resources.password);
+        }
+        catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+        }
+    }
+    @Override
+    public void oppretteBeskjed(Beskjed beskjed) {
+        String opprettBeskjedQuery = "Insert into Beskjeder (beskrivelse, dato_tid, synlig_tid) Values (?, ?, ?)";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(opprettBeskjedQuery)) {
+            preparedStatement.setString(1, beskjed.getBeskrivelse());
+            preparedStatement.setObject(2, beskjed.getDatoOgTid());
+            preparedStatement.setInt(3, beskjed.getSynligTidsenhet());
+            preparedStatement.executeUpdate();
+        }
+        catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+        }
+    }
+}
