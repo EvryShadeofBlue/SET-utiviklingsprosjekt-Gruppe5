56772796016f0c938321ea5df0eebc6305ce90db
Aram <aramf@hiof.no>
2024-11-05 18:38:45 +0100

    added query for getting, deleting and editing an avtale

--- App/core/src/main/java/org/app/core/avtaler/AvtaleDBImplementation.java
+++ App/core/src/main/java/org/app/core/avtaler/AvtaleDBImplementation.java
@@ -1,9 +1,12 @@
 package org.app.core.avtaler;
 
+import org.app.core.models.Parorende;
+import org.app.core.models.Pleietrengende;
 import org.app.core.models.Resources;
 import org.app.core.repository.AvtaleRepository;
 
 import java.sql.*;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class AvtaleDBImplementation implements AvtaleRepository {
@@ -48,17 +51,66 @@ public class AvtaleDBImplementation implements AvtaleRepository {
 
     @Override
     public void oppdaterAvtale(Avtale avtale) {
+        String oppdaterAvtaleQuery = "update Avtaler set beskrivelse = ?, dato_og_tid = ?, gjentakelse = ?, slutt_dato = ?, pleietrengende_id = ?, parorende_id = ? " +
+                "where avtale_id = ?";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(oppdaterAvtaleQuery)) {
+            preparedStatement.setString(1, avtale.getBeksrivelse());
+            preparedStatement.setObject(2, avtale.getDatoOgTid());
+            preparedStatement.setString(3, avtale.getGjentakelse());
+            preparedStatement.setObject(4, avtale.getSluttDato());
+            preparedStatement.setInt(5, avtale.getParorende().getParorendeId());
+            preparedStatement.setInt(6, avtale.getPleietrengende().getPleietrengendeId());
+            preparedStatement.executeUpdate();
+        }
+        catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void slettAvtale(int avtaleId) {
+        String sletteAvtaleQuery = "delete from Avtaler where avtale_id = ?";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sletteAvtaleQuery)) {
+            preparedStatement.setInt(1, avtaleId);
+            preparedStatement.executeUpdate();
+        }
+        catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+        }
 
     }
 
     @Override
     public Avtale hentAvtale(int avtaleId) {
+        String hentAvtaleQuery = "select avtale_id, beskrivelse, dato_og_tid, gjentakelse, slutt_dato, pleietrengende_id, parorende_id " +
+                "from Avtaler where avtale_id = ?";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(hentAvtaleQuery)) {
+            preparedStatement.setInt(1, avtaleId);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                int id = resultSet.getInt("avtale_id");
+                String beskrivelse = resultSet.getString("beskrivelse");
+                LocalDateTime datoOgTid = resultSet.getObject("dato_og_tid", LocalDateTime.class);
+                String gjentakelse = resultSet.getString("gjentakelse");
+                LocalDateTime sluttdato = resultSet.getObject("slutt_dato", LocalDateTime.class);
+                int parorendeId = resultSet.getInt("parorende_id");
+                int pleietrengendeId = resultSet.getInt("pleietrengende_id");
+
+                Parorende parorende = new Parorende();
+                parorende.setParorendeId(parorendeId);
+                Pleietrengende pleietrengende = new Pleietrengende();
+                pleietrengende.setPleietrengendeId(pleietrengendeId);
+
+                Avtale avtale = new Avtale(id, datoOgTid, beskrivelse, gjentakelse, sluttdato, pleietrengende, parorende);
+                return avtale;
+            }
+        }
+        catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+        }
         return null;
-    }
-
-    @Override
-    public void slettAvtale(int avtaleId) {
-
     }
 
     @Override
