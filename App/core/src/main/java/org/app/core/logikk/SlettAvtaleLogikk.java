a90fff67d1affe40301486450cd738111ba6f4a7
Aram <aramf@hiof.no>
2024-11-16 21:04:18 +0100

    separert koden for sletting av avtaler i en egen klasse

---
+++ App/core/src/main/java/org/app/core/logikk/SlettAvtaleLogikk.java
@@ -0,0 +1,24 @@
+package org.app.core.logikk;
+
+import org.app.core.repositories.AvtaleRepository;
+import org.app.core.models.Avtale;
+
+public class SlettAvtaleLogikk {
+    private AvtaleRepository avtaleRepository;
+
+    public SlettAvtaleLogikk(AvtaleRepository avtaleRepository) {
+        this.avtaleRepository = avtaleRepository;
+    }
+
+    public boolean slettAvtale(int avtaleId) {
+        Avtale avtale = avtaleRepository.hentAvtale(avtaleId);
+
+        if (avtale == null) {
+            System.out.println("Avtalen ble ikke funnet.");
+            return false;
+        }
+
+        avtaleRepository.slettAvtale(avtaleId);
+        return true;
+    }
+}
