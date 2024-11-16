4538fab466d0edfb0fd905acb4c76be2f58be5b6
Aram <aramf@hiof.no>
2024-11-16 21:04:08 +0100

    separert koden for visning av avctaler i en egen klasse

---
+++ App/core/src/main/java/org/app/core/logikk/VisAvtaleLogikk.java
@@ -0,0 +1,24 @@
+package org.app.core.logikk;
+
+import org.app.core.models.Avtale;
+import org.app.core.models.Parorende;
+import org.app.core.repositories.AvtaleRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class VisAvtaleLogikk {
+    private AvtaleRepository avtaleRepository;
+    public VisAvtaleLogikk(AvtaleRepository avtaleRepository) {
+        this.avtaleRepository = avtaleRepository;
+    }
+
+    public List<Avtale> hentAvtalerForParorende(Parorende parorende) {
+        List<Avtale> avtaler = avtaleRepository.hentAvtalerForParorende(parorende);
+
+        List<Avtale> sorterteAvtaler = avtaler.stream()
+                .sorted((avtale1, avtale2) -> avtale2.getDatoOgTid().compareTo(avtale1.getDatoOgTid()))
+                .collect(Collectors.toList());
+        return sorterteAvtaler;
+    }
+}
