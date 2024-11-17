06c4495249c9b60bc43fad748eb866be3398837d
Aram <aramf@hiof.no>
2024-11-17 20:23:05 +0100

    La til test klasse for å gjennomføre tester

---
+++ App/tests/src/test/java/org/app/tests/usercases/avtaler/opprettAvtale/feil/OpprettMånedligAvtaleFeilTest.java
@@ -0,0 +1,20 @@
+package org.app.tests.usercases.avtaler.opprettAvtale.feil;
+
+import org.app.core.models.Parorende;
+import org.app.core.models.Pleietrengende;
+import org.app.core.repositories.AvtaleRepository;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class OpprettMånedligAvtaleFeilTest {
+    @Mock
+    AvtaleRepository mockAvtaleRepo;
+
+    @Mock
+    Parorende mockParorende;
+
+    @Mock
+    Pleietrengende mockPleietrengende;
+}
