a5c06fda5619a30069cb7d4f7c6a5feb9c51abe6
Aram <aramf@hiof.no>
2024-11-19 20:16:07 +0100

    opprettet klasse for testing av sletting av en avtale

---
+++ App/tests/src/test/java/org/app/tests/usercases/avtaler/slettAvtale/SlettAvtaleTest.java
@@ -0,0 +1,22 @@
+package org.app.tests.usercases.avtaler.slettAvtale;
+
+import org.app.core.models.Parorende;
+import org.app.core.models.Pleietrengende;
+import org.app.core.repositories.AvtaleRepository;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class SlettAvtaleTest {
+    @Mock
+    AvtaleRepository mockAvtaleRepo;
+
+    @Mock
+    Parorende mockParorende;
+
+    @Mock
+    Pleietrengende mockPleietrengende;
+
+
+}
