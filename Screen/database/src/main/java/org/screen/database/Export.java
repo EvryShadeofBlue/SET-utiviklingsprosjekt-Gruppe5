9a5576a8c66bcf2d495e3cbd6d75c1d578405817
Damagedjokke <thgl0l@hotmail.com>
2024-11-25 18:27:06 +0100

    screen avtaler time only

--- Screen/database/src/main/java/org/screen/database/Export.java
+++ Screen/database/src/main/java/org/screen/database/Export.java
@@ -52,7 +52,7 @@ public class Export implements DataExportInterface {
     @Override
     public List<Avtale> exportAvtalerToday(int pleietrengende_id) {
         List<Avtale> avtaleList = new ArrayList<>();
-        String exportQuery = "SELECT beskrivelse, dato_og_tid " +
+        String exportQuery = "SELECT beskrivelse, TIME(dato_og_tid) AS tid " +
                 "FROM Avtaler " +
                 "WHERE DATE(dato_og_tid) = CURDATE() " +
                 "AND pleietrengende_id = " + pleietrengende_id + " " +
@@ -65,7 +65,7 @@ public class Export implements DataExportInterface {
 
             while (rs.next()) {
                 String description = rs.getString("beskrivelse");
-                String dateTime = rs.getString("dato_og_tid");
+                String dateTime = rs.getString("tid");
 
                 Avtale avtaler = new Avtale(description, dateTime);
                 avtaleList.add(avtaler);
@@ -86,9 +86,9 @@ public class Export implements DataExportInterface {
     @Override
     public List<Avtale> exportAvtalerTomorrow(int pleietrengende_id) {
         List<Avtale> avtaleList = new ArrayList<>();
-        String exportQuery = "SELECT beskrivelse, dato_og_tid " +
+        String exportQuery = "SELECT beskrivelse, TIME(dato_og_tid) AS tid " +
                 "FROM Avtaler " +
-                "WHERE DATE(dato_og_tid) = date_add(CURDATE(), interval 1 day) " +
+                "WHERE DATE(dato_og_tid) = DATE_ADD(CURDATE(), INTERVAL 1 DAY) " +
                 "AND pleietrengende_id = " + pleietrengende_id + " " +
                 "ORDER BY dato_og_tid ASC;";
 
@@ -99,7 +99,7 @@ public class Export implements DataExportInterface {
 
             while (rs.next()) {
                 String description = rs.getString("beskrivelse");
-                String dateTime = rs.getString("dato_og_tid");
+                String dateTime = rs.getString("tid");
 
                 Avtale avtaler = new Avtale(description, dateTime);
                 avtaleList.add(avtaler);
